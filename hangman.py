0cfd4c2f5ea4033ec8b3967b08fcaabf556e8c2b
John Han <sirjohn146@gmail.com>
2022-11-26 16:04:26 -0600

    Finished unittest for guess method. Added replay functionality, score keeping, and word repeat protection to driver.

--- hangman.py
+++ hangman.py
@@ -26,6 +26,7 @@ class Hangman():
         self.guessed_correct = []
         self.mistakes_left = 7
         self.fill_board(self.secret_word, self.board)
+        self.query_on = False
 
     # runs a check between guessed and board to update 'show' values on board
     # also updates mistakes_left
@@ -69,7 +70,8 @@ class Hangman():
         current_guess = input('\nWhat is your guess?\nLetter(s) only. No numbers, special characters, or spaces.\n').lower()
         for n in current_guess:
             if not n.isalpha():
-                return print("\nYou've entered something invalid. Please try again.")
+                print("\nYou've entered something invalid. Please try again.")
+                return 'Invalid'
 
         current_guess = list(set(current_guess))
 
@@ -88,36 +90,82 @@ class Hangman():
                     print(f'\nOops! Looks like you have used up all your guesses.\nThe letter "{n}" was not guessed.')
                     break
             
-
+           
 def driver():
-    hangman.show_board()
-    while hangman.mistakes_left > 0:
-        if len(hangman.guessed_correct) == hangman.secret_unique_chars:
-            break
-        
-        else:
-            hangman.guess()
+    options = {
+        'yes': ['y', 'yes', 'ye', 'yeah', 'yea', 'yeh', 'ya', 'yah', '(y)es', '(y)', 'play', 'go', 'continue', 'cont', 'more'],
+        'no': ['n', 'no', 'nah', 'na', 'nay', '(n)o', '(n)', 'quit', 'stop', 'exit']
+    }
+    wins = 0
+    losses = 0
+    two_words_ago = ''
+    last_word_used = ''
+    
+    # overall loop for the game to go back to if the user wants to play another round
+    game_running = True
+    while game_running:
+        # if the secret word is either of the last 2 words used, this loops through randomization until it finds a word that is not the last word used or was used two words ago
+        while hangman.secret_word == two_words_ago or hangman.secret_word == last_word_used:
+            hangman.secret_word = random.choice(hangman.word_bank)
+
+        # loop shows board then checks to see if win condition is met which will break loop
+        # if not met, runs guess method then loops again
+        while hangman.mistakes_left > 0:
             hangman.show_board()
+            if len(hangman.guessed_correct) == hangman.secret_unique_chars:
+                break
+            
+            else:
+                hangman.guess()
 
-    # when victory condition met or mistakes_left goes down to 0
-    print('\n')
-    print('=~'*40)
-    print("\nLet's see how you did...")
+        # when victory condition met or mistakes_left goes down to 0
+        print('\n')
+        print('=~'*40)
+        print("\nLet's see how you did...")
 
-    if len(hangman.guessed_correct) == hangman.secret_unique_chars:
+        # win
+        if len(hangman.guessed_correct) == hangman.secret_unique_chars:
+                wins += 1
+                print('\n')
+                print('=~'*40)
+                hangman.show_board_short()
+                print(f'\nYou win! You got the word "{hangman.secret_word}" correctly with {hangman.mistakes_left} guess(es) left.')
+                print(f'\nYour overall score is: {wins}W - {losses}L\n')
+                print('=~'*40)
+        
+        # loss
+        else:
+            losses += 1
             print('\n')
             print('=~'*40)
             hangman.show_board_short()
-            print(f'You win! You got the word "{hangman.secret_word}" correctly with {hangman.mistakes_left} guess(es) left.')
+            print(f'\nThe secret word was: "{hangman.secret_word}"')
+            print(f"\nYou got {len(hangman.guessed_correct)} letters out of {hangman.secret_unique_chars}. Better luck next time!")
+            print(f'\nYour overall score is: {wins}W - {losses}L\n')
             print('=~'*40)
-    
-    else:
-        print('\n')
-        print('=~'*40)
-        hangman.show_board_short()
-        print(f'\nThe secret word was: "{hangman.secret_word}"')
-        print(f"\nYou got {len(hangman.guessed_correct)} letters out of {hangman.secret_unique_chars}. Better luck next time!")
-        print('=~'*40)
+
+        # asking the user if they want to play another round or not
+        hangman.query_on = True
+        while hangman.query_on:
+            reset_query = input('\nWould you like to play again?\n(Y)es | (N)o\n').lower()
+            if reset_query in options['yes']:
+                # update both previous words
+                two_words_ago = ''+last_word_used
+                last_word_used = ''+hangman.secret_word
+                print('\n')
+                print('=~'*40)
+                hangman.__init__()
+
+            elif reset_query in options['no']:
+                print('\n')
+                print('=~'*40)
+                print(f'\nYour final overall score is: {wins}W - {losses}L\nThanks for playing!\n')
+                print('=~'*40)
+                game_running = False
+                hangman.query_on = False
+
+            else:
+                print(f'\n"{reset_query}" is an invalid response. Please try again.')
         
 
 hangman = Hangman()
--- test.py
+++ test.py
@@ -13,6 +13,7 @@ class HangTest(unittest.TestCase):
         self.assertIsNotNone(test_board)
 
     def test_init(self):
+        hangtest.__init__()
         self.assertIsInstance(hangtest.secret_word, str)
         self.assertIsInstance(hangtest.mistakes_left, int)
         self.assertIs(hangtest.mistakes_left, 7)
@@ -21,10 +22,11 @@ class HangTest(unittest.TestCase):
         self.assertEqual(len(hangtest.guessed), 0)
 
     def test_update(self):
+        hangtest.__init__()
         hangtest.board = {}
+        hangtest.guessed = ['t', 'a']
         hangtest.fill_board('test', hangtest.board)
         self.assertEqual(len(hangtest.board), 4)
-        hangtest.guessed = ['t', 'a']
         self.assertEqual(len(hangtest.guessed), 2)
         hangtest.update()
         self.assertEqual(hangtest.mistakes_left, 6)
@@ -34,20 +36,62 @@ class HangTest(unittest.TestCase):
         self.assertTrue(hangtest.board[4]['show'])
 
     def test_show(self):
+        hangtest.__init__()
         hangtest.board = {}
+        hangtest.guessed = ['t', 'a']
         hangtest.fill_board('test', hangtest.board)
         self.assertEqual(len(hangtest.board), 4)
-        hangtest.guessed = ['t', 'a']
         self.assertEqual(len(hangtest.guessed), 2)
         hangtest.show_board()
         self.assertEqual(hangtest.mistakes_left, 6)
         self.assertEqual(hangtest.board_str, 't _ _ t ')
 
-    # last test to get working, need to figure out how patch works
-    # @patch('builtins.input', return_value='green80')
-    # def test_guess_invalid1(self, mock_input):
-    #     result = hangtest.guess()
-    #     self.assertEqual(result, "\nYou've entered something invalid. Please try again.")
+    # working case
+    @patch('builtins.input', return_value='ta')
+    def test_guess_valid(self, input):
+        hangtest.__init__()
+        hangtest.board = {}
+        hangtest.fill_board('test', hangtest.board)
+        self.assertEqual(len(hangtest.board), 4)
+        hangtest.guess()
+        self.assertIn('t', hangtest.guessed)
+        self.assertIn('a', hangtest.guessed)
+        hangtest.show_board()
+        self.assertEqual(hangtest.mistakes_left, 6)
+        self.assertEqual(hangtest.board_str, 't _ _ t ')
+
+    # semi-working case that tries 3 guesses at once when there is only 1 guess left
+    @patch('builtins.input', return_value='dan')
+    def test_guess_semi_valid(self, input):
+        hangtest.__init__()
+        hangtest.board = {}
+        hangtest.guessed = ['x', 'y', 'z', 'q', 'w', 'v']
+        hangtest.fill_board('test', hangtest.board)
+        self.assertEqual(len(hangtest.board), 4)
+        self.assertEqual(len(hangtest.guessed), 6)
+        hangtest.guess()
+        self.assertEqual(len(hangtest.guessed), 7)
+
+    # invalid case with numbers
+    @patch('builtins.input', return_value='80')
+    def test_guess_invalid1(self, input):
+        hangtest.__init__()
+        self.assertEqual(hangtest.guess(), 'Invalid')
+        self.assertEqual(len(hangtest.guessed), 0)
+
+    # invalid case with special chars
+    @patch('builtins.input', return_value='!@')
+    def test_guess_invalid2(self, input):
+        hangtest.__init__()
+        self.assertEqual(hangtest.guess(), 'Invalid')
+        self.assertEqual(len(hangtest.guessed), 0)
+
+    # invalid case with letters, numbers, and special chars
+    @patch('builtins.input', return_value='green80!@')
+    def test_guess_invalid3(self, input):
+        hangtest.__init__()
+        self.assertEqual(hangtest.guess(), 'Invalid')
+        self.assertEqual(len(hangtest.guessed), 0)
 
     
 unittest.main()
